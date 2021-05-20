1b47a507d2cf0a50cb60413615623b5632bafcf4
Arpad G. Bondor <arpad.g.bondor@gmail.com>
2021-05-20 12:44:20 +0100

    20/05/2021 solutions

---
+++ 0010.js
@@ -0,0 +1,41 @@
+// Build Tower
+// Build Tower by the following given argument:
+// number of floors (integer and always greater than 0).
+
+// Tower block is represented as *
+
+// Python: return a list;
+// JavaScript: returns an Array;
+// C#: returns a string[];
+// PHP: returns an array;
+// C++: returns a vector<string>;
+// Haskell: returns a [String];
+// Ruby: returns an Array;
+// Lua: returns a Table;
+// Have fun!
+
+// for example, a tower of 3 floors looks like below
+
+// [
+//   '  *  ',
+//   ' *** ',
+//   '*****'
+// ]
+// and a tower of 6 floors looks like below
+
+// [
+//   '     *     ',
+//   '    ***    ',
+//   '   *****   ',
+//   '  *******  ',
+//   ' ********* ',
+//   '***********'
+// ]
+
+function towerBuilder(nFloors) {
+  const tower = new Array(nFloors);
+  for (let i = 0; i < tower.length; ++i) {
+    tower[i] = ''.padStart(nFloors - i - 1) + ''.padStart(2 * i + 1, '*') + ''.padStart(nFloors - i - 1);
+  }
+  return tower;
+}
