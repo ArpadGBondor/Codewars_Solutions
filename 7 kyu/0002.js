1bf055124f72ee26f0cae1f856438ecee48f37d2
Arpad G. Bondor <arpad.g.bondor@gmail.com>
2021-05-24 00:29:39 +0100

    24/05/2021 solutions

--- 7 kyu/0002.js
+++ 7 kyu/0002.js
@@ -8,6 +8,11 @@
 // are actual quotes from Jaden Smith, but they are not capitalized in the same way he
 // originally typed them.
 
+// Example:
+
+// Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
+// Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
+
 String.prototype.toJadenCase = function () {
   return this.split(' ')
     .map((word) => word.substring(0, 1).toUpperCase() + word.substring(1))
