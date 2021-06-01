06fa290f5d4593c4a297442d612b679427d8e2a2
Arpad G. Bondor <arpad.g.bondor@gmail.com>
2021-06-01 07:08:58 +0100

    01/06/2021 solutions

--- 3 kyu/0002.js
+++ 3 kyu/0002.js
@@ -9,11 +9,27 @@
 // Remember about the order of operations! Multiplications and divisions have a higher priority and should be performed left-to-right. Additions and subtractions have a lower priority and should also be performed left-to-right.
 
 const Calculator = function () {
+  this.executeOperation = (string, operatorIndex) => {
+    const operation = {
+      '+': (a, b) => a + b,
+      '-': (a, b) => a - b,
+      '*': (a, b) => a * b,
+      '/': (a, b) => a / b,
+    };
+    // solve the left and the right side recursively
+    let leftOperand = this.evaluate(string.substr(0, operatorIndex)),
+      rightOperand = this.evaluate(string.substr(operatorIndex + 1)),
+      operator = string[operatorIndex];
+    // execute the operator between the sides
+    console.log(`${string} => ${operation[operator](leftOperand, rightOperand)}`);
+    return operation[operator](leftOperand, rightOperand);
+  };
+
   this.evaluate = (string) => {
     // Handle all parenthesis first
     let openingParenthesis = string.indexOf('(');
     while (openingParenthesis > -1) {
-      // Find the pair of the opening parenthesis
+      // Find the closing pair of the opening parenthesis
       let closingParenthesis = openingParenthesis + 1,
         numberOfOpeningParenthesis = 1;
       while (string[closingParenthesis] !== ')' || numberOfOpeningParenthesis > 1) {
@@ -21,7 +37,7 @@ const Calculator = function () {
         if (string[closingParenthesis] === ')') --numberOfOpeningParenthesis;
         ++closingParenthesis;
       }
-      // Solve the section between the parenthesis recursively
+      // Solve the section between the opening and closing parenthesis recursively
       string =
         string.substr(0, openingParenthesis) +
         `${this.evaluate(string.substr(openingParenthesis + 1, closingParenthesis - openingParenthesis - 1))}` +
@@ -29,39 +45,18 @@ const Calculator = function () {
       // Check if we have more opening parenthesis
       openingParenthesis = string.indexOf('(');
     }
-
     // Additions and subtractions
     // Find the last addition operator
     // check for extra space at subtractions, to avoid confusion with negative numbers
     let additionOperator = Math.max(string.lastIndexOf('+'), string.lastIndexOf('- '));
     if (additionOperator > -1) {
-      // solve the left and the right side recursively
-      let leftOperand = this.evaluate(string.substr(0, additionOperator)),
-        rightOperand = this.evaluate(string.substr(additionOperator + 1));
-      // execute the operator between the sides
-      if (string[additionOperator] === '+') {
-        console.log(`${string} => ${leftOperand + rightOperand}`);
-        return leftOperand + rightOperand;
-      } else {
-        console.log(`${string} => ${leftOperand - rightOperand}`);
-        return leftOperand - rightOperand;
-      }
+      return this.executeOperation(string, additionOperator);
     } else {
       // if there's no addition operator, check for  multiplication and division
-      // Find the last addition operator
+      // Find the last multiplication operator
       let multiplicationOperator = Math.max(string.lastIndexOf('*'), string.lastIndexOf('/'));
       if (multiplicationOperator > -1) {
-        // solve the left and the right side recursively
-        let leftOperand = this.evaluate(string.substr(0, multiplicationOperator)),
-          rightOperand = this.evaluate(string.substr(multiplicationOperator + 1));
-        // execute the operator between the sides
-        if (string[multiplicationOperator] === '*') {
-          console.log(`${string} => ${leftOperand * rightOperand}`);
-          return leftOperand * rightOperand;
-        } else {
-          console.log(`${string} => ${leftOperand / rightOperand}`);
-          return leftOperand / rightOperand;
-        }
+        return this.executeOperation(string, multiplicationOperator);
       } else {
         // if there are no operators, just return the value
         console.log(`${string} => ${parseFloat(string)}`);
