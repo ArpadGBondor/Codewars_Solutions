4ae15891f3ac777cb0ddfcd6254dcfc8b64466d6
Arpad G. Bondor <arpad.g.bondor@gmail.com>
2021-06-06 05:31:55 +0100

    06/06/2021 solutions

--- 4 kyu/0007.js
+++ 4 kyu/0007.js
@@ -21,50 +21,30 @@
 // Focus attention on efficiency
 
 function dblLinear(n) {
-  const u = [1];
-  let index = 0,
-    insertIndex = 0;
+  const u = [1],
+    f2n = (n) => 2 * n + 1,
+    f3n = (n) => 3 * n + 1;
+  let ptr2n = 0,
+    ptr3n = 0;
+  let _2n = f2n(u[ptr2n]),
+    _3n = f3n(u[ptr3n]);
 
-  while (insertIndex < n) {
-    // We need to find where to insert the y=2x+1 elements to keep the array sorted
-    let insert = 2 * u[index] + 1;
-    // logaritmic search for the place of insertion.
-    insertIndex = 1;
-    let [start, end] = [0, u.length - 1];
-    // if new element greater than the last of the array
-    if (u[end] < insert) {
-      u.push(insert);
+  for (let i = 0; i < n; ++i) {
+    if (_2n === _3n) {
+      u.push(_2n);
+      ++ptr2n;
+      _2n = f2n(u[ptr2n]);
+      ++ptr3n;
+      _3n = f3n(u[ptr3n]);
+    } else if (_2n < _3n) {
+      u.push(_2n);
+      ++ptr2n;
+      _2n = f2n(u[ptr2n]);
     } else {
-      // while [start,end] interval longer than 1
-      while (start < end) {
-        // half the interval
-        insertIndex = Math.floor((start + end) / 2);
-        if (u[insertIndex] === insert) {
-          // if new element is already in the array
-          start = end = insertIndex;
-        } else if (u[insertIndex] < insert) {
-          if (start + 1 === end) {
-            // if interval is [n, n+1] only two numbers, we want insertIndex
-            // to point to the last element of the interval, which is greater than insert
-            start = insertIndex = end;
-          } else {
-            // new element is greater than the element in the middle,
-            // so check the upper half of the interval next.
-            start = insertIndex;
-          }
-        } else {
-          // new element is lesser than the element in the middle,
-          // so check the lower half of the interval next.
-          end = insertIndex;
-        }
-      }
-      // if new element is already in the array do not insert
-      if (u[insertIndex] !== insert) u.splice(insertIndex, 0, insert);
+      u.push(_3n);
+      ++ptr3n;
+      _3n = f3n(u[ptr3n]);
     }
-
-    // 3x + 1 will always be greater than the last element, just push it.
-    u.push(3 * u[index] + 1);
-    ++index;
   }
 
   return u[n];
