05acba9646975e1b4f5ba287f4bc52ae838f23fc
Arpad G. Bondor <arpad.g.bondor@gmail.com>
2021-06-20 11:31:40 +0100

    20/06/21021 solutions

---
+++ C#/6 kyu/0010.cs
@@ -0,0 +1,29 @@
+// Kata: Counting Duplicates
+// Link: https://www.codewars.com/kata/54bf1c2cd5b56cc47f0007a1
+
+// Count the number of Duplicates
+// Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+
+// Example
+// "abcde" -> 0 # no characters repeats more than once
+// "aabbcde" -> 2 # 'a' and 'b'
+// "aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+// "indivisibility" -> 1 # 'i' occurs six times
+// "Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+// "aA11" -> 2 # 'a' and '1'
+// "ABBA" -> 2 # 'A' and 'B' each occur twice
+
+using System;
+using System.Linq;
+
+public class Kata
+{
+  public static int DuplicateCount(string str)
+  {
+    var query = from s in str.ToLower()
+                group s by s into g
+                let count = g.Count()
+                select new {Value = g.Key, Count = count};
+    return query.Count(q=>q.Count >= 2);
+  }
+}
