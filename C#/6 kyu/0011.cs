a4c34ed9e4853c7dd8fa61a117d5ee16c640fb69
Arpad G. Bondor <arpad.g.bondor@gmail.com>
2021-06-21 11:11:23 +0100

    21/06/2021 solutions

---
+++ C#/6 kyu/0011.cs
@@ -0,0 +1,77 @@
+// Kata: Prize Draw
+// Link: https://www.codewars.com/kata/5616868c81a0f281e500005c
+
+// To participate in a prize draw each one gives his/her firstname.
+
+// Each letter of a firstname has a value which is its rank in the English alphabet. A and a have rank 1, B and b rank 2 and so on.
+
+// The length of the firstname is added to the sum of these ranks hence a number som.
+
+// An array of random weights is linked to the firstnames and each som is multiplied by its corresponding weight to get what they call a winning number.
+
+// Example:
+// names: "COLIN,AMANDBA,AMANDAB,CAROL,PauL,JOSEPH"
+// weights: [1, 4, 4, 5, 2, 1]
+
+// PauL -> som = length of firstname + 16 + 1 + 21 + 12 = 4 + 50 -> 54
+// The *weight* associated with PauL is 2 so PauL's *winning number* is 54 * 2 = 108.
+// Now one can sort the firstnames in decreasing order of the winning numbers. When two people have the same winning number sort them alphabetically by their firstnames.
+
+// Task:
+// parameters: st a string of firstnames, we an array of weights, n a rank
+
+// return: the firstname of the participant whose rank is n (ranks are numbered from 1)
+
+// Example:
+// names: "COLIN,AMANDBA,AMANDAB,CAROL,PauL,JOSEPH"
+// weights: [1, 4, 4, 5, 2, 1]
+// n: 4
+
+// The function should return: "PauL"
+// Notes:
+// The weight array is at least as long as the number of names, it may be longer.
+
+// If st is empty return "No participants".
+
+// If n is greater than the number of participants then return "Not enough participants".
+
+// See Examples Test Cases for more examples.
+
+using System;
+using System.Linq;
+
+public class Rank
+{
+    public static string NthRank(string st, int[] we, int n)
+    {
+      if (String.IsNullOrEmpty(st)) {
+        return "No participants";
+      }
+      
+      string[] names = st.Split(',');
+      
+      if (n > names.Length) 
+      {
+        return "Not enough participants";
+      }
+      
+      int [] som = new int[names.Length];
+      for (int i = 0; i < names.Length; ++i) 
+      {
+        som[i] = names[i].Length;
+        foreach (char c in names[i].ToLower())  
+        {
+           som[i] += -96 + (int) c; 
+        }
+        som[i] *= we[i];
+      }
+      
+      var query = names
+         .Select((name,index) => new {Name = name, Score = som[index]})
+         .OrderByDescending(q=>q.Score).ThenBy(q=>q.Name)
+         .Skip(n-1).First();
+      
+      return query.Name;
+      
+    }
+}
