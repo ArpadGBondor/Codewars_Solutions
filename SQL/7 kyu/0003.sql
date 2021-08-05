00c87a54f419fb23599ff64685c83a5a553c552c
Arpad G. Bondor <arpad.g.bondor@gmail.com>
2021-08-05 20:04:10 +0100

    05/08/2021 solutions

---
+++ SQL/7 kyu/0003.sql
@@ -0,0 +1,13 @@
+-- Kata: SQL Basics: Raise to the Power
+-- Link: https://www.codewars.com/kata/594a8f653b5b4e8f3d000035
+
+-- Given the following table 'decimals':
+
+-- decimals table schema
+-- id
+-- number1
+-- number2
+-- Return a table with a single column result which is the output of number1 raised to the power of number2.
+
+select pow(number1,number2) as result
+from decimals
