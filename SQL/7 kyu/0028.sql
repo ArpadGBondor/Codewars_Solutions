01983e57c6149d48f2777fe876d4f6d5918e6234
Arpad G. Bondor [Gabriel] <arpad.g.bondor@gmail.com>
2025-02-03 02:07:00 +0000

    03/02/2025 solutions

---
+++ SQL/7 kyu/0028.sql
@@ -0,0 +1,7 @@
+-- Kata: Sum of angles
+-- Link: https://www.codewars.com/kata/5a03b3f6a1c9040084001765
+
+-- Find the total sum of internal angles (in degrees) in an n-sided simple
+-- polygon. N will be greater than 2.
+
+select (n-2)*180 as res from angle;
