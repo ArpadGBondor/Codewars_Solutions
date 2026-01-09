2aa4810be218e0f15dee00cdb6ce8509e7fe23ad
Arpad G. Bondor [Gabriel] <arpad.g.bondor@gmail.com>
2026-01-09 23:49:38 +0000

    09/01/2026 solutions

---
+++ TypeScript/5 kyu/0005.ts
@@ -0,0 +1,23 @@
+// Kata: Count IP Addresses
+// Link: https://www.codewars.com/kata/526989a41034285187000de4
+
+// Implement a function that receives two IPv4 addresses, and returns the number
+// of addresses between them (including the first one, excluding the last one).
+
+// All inputs will be valid IPv4 addresses in the form of strings. The last
+// address will always be greater than the first one.
+
+// Examples
+// * With input "10.0.0.0", "10.0.0.50"  => return   50
+// * With input "10.0.0.0", "10.0.1.0"   => return  256
+// * With input "20.0.0.10", "20.0.1.0"  => return  246
+
+export function ipsBetween(start: string, end: string): number {
+  const toNumbers = (s: string) =>
+    s.split('.').map((s: string): number => Number(s));
+  const difference = (a1: number[], a2: number[]): number[] =>
+    a1.map((n: number, i: number): number => n - a2[i]);
+  const sum = (a: number[]): number =>
+    a.reduce((sum: number, current: number): number => 256 * sum + current, 0);
+  return sum(difference(toNumbers(end), toNumbers(start)));
+}
