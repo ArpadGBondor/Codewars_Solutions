f289e675116510177e512e369734969e2d3958e4
Arpad G. Bondor [Gabriel] <arpad.g.bondor@gmail.com>
2026-01-12 08:47:10 +0000

    12/01/2026 solutions

---
+++ TypeScript/6 kyu/0008.ts
@@ -0,0 +1,26 @@
+// Kata: Who likes it?
+// Link: https://www.codewars.com/kata/5266876b8f4bf2da9b000362
+
+// Digital root is the recursive sum of all the digits in a number.
+
+// Given n, take the sum of the digits of n. If that value has more than one
+// digit, continue reducing in this way until a single-digit number is produced.
+// The input will be a non-negative integer.
+
+// Examples
+//     16  -->  1 + 6 = 7
+//    942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+// 132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+// 493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+
+export const digitalRoot = (n: number): number => {
+  let result: number = n;
+  while (result >= 10) {
+    result = result
+      .toString()
+      .split('')
+      .map(Number)
+      .reduce((a: number, b: number): number => a + b, 0);
+  }
+  return result;
+};
